b8062b59ff8b1b5a87aaeb8813347f1a40d807b8
Austin-GitHub <84061326+Austin2gitgub@users.noreply.github.com>
2025-03-27 13:11:52 -0500

    Update page.tsx

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -85,7 +85,7 @@ export default function DataPage() {
 
               <Card className="md:col-span-2 shadow-lg hover:shadow-xl transition-all duration-300">
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">Lead Levels by Neighborhood</CardTitle>
+                  <CardTitle className="text-sm font-medium">Spatial Regression for 90th Percentile of Max Lead Draw​</CardTitle>
                   <LineChart className="h-4 w-4 text-muted-foreground" />
                 </CardHeader>
                 <CardContent>
